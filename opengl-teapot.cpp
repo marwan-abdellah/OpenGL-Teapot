4f68e7f938649840955fed0383d64c0b6130c101
marwan-abdellah <abdellah.marwan@gmail.com>
2015-05-20 20:48:19 +0200

    Final fixes and pushing the code to GitHub.

--- opengl-teapot.cpp
+++ opengl-teapot.cpp
@@ -5,6 +5,9 @@
 #include <math.h>
 #include <unistd.h>
 
+#define WINDOW_WIDTH 512
+#define WINDOW_HEIGHT 512
+
 // X-rotation
 GLfloat xRot = 0.f;
 
@@ -13,11 +16,27 @@ GLfloat yRot = 0.f;
 
 // Z-rotation
 GLfloat zRot = 0.f;
+\
+// X-rotation CUBE
+GLfloat xRotCube= 0.f;
+
+// Y-rotation CUBE
+GLfloat yRotCube = 0.f;
+
+// Z-rotation CUBE
+GLfloat zRotCube = 0.f;
 
 // Teapot size
 GLdouble size = 1;
 
+// FBO
+GLuint fboId;
 
+// Texture attached to the FBO
+GLuint fboTexId;
+
+// Render to FBO
+bool renderOnbox = true;
 
 /******************************************************************************/
 // Prototypes
@@ -40,6 +59,37 @@ float randf()
 }
 
 /**
+ * @brief initializeFBO
+ */
+void initializeFBO()
+{
+    // Generate the FBO and assign an ID to it
+    glGenFramebuffers(1, &fboId);
+
+    // Bind the FBO to set its properties
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+
+    // Generate the texture we're going to render the scene to
+    glGenTextures(1, &fboTexId);
+
+    // "Bind" the newly created texture.
+    glBindTexture(GL_TEXTURE_2D, fboTexId);
+
+    // Give an empty image to OpenGL ( the last "0" ) to allocate the texture
+    // on the GPU.
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 WINDOW_WIDTH, WINDOW_HEIGHT,
+                 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+    // Poor filtering. Needed !
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // Set "renderedTexture" as our colour attachement #0
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, fboTexId, 0);
+}
+
+/**
  * @brief initialize
  * @param argc
  * @param argv
@@ -53,12 +103,12 @@ void initialize(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGB);
 
     // Window size
-    glutInitWindowSize(400,350);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
     // Create the window
     glutCreateWindow("Teapot FBO");
 
-    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
 
     // Clear the buffer color
     glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -68,6 +118,12 @@ void initialize(int argc, char** argv)
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
     glutIdleFunc(idle);
+
+    // GLEW initialization
+    glewInit();
+
+    // Initializtion FBO
+    // initializeFBO();
 }
 
 /**
@@ -75,11 +131,19 @@ void initialize(int argc, char** argv)
  */
 void renderTeapot()
 {
+    // Clear color of the FBO
+    glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+
+    // Clear Screen And Depth Buffer on the framebuffer to black
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     // Reset the matrix
     glMatrixMode(GL_MODELVIEW);
 
     // Clean the color buffer
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnable(GL_DEPTH_TEST);
 
     // Load the I
     glLoadIdentity();
@@ -100,7 +164,185 @@ void renderTeapot()
 
     // Draw the teapot
     glutSolidTeapot(size);
+}
 
+/**
+ * @brief renderToFBO
+ */
+void renderToFBO()
+{
+    // Bind the FBO to render to it
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+
+    // Render on the whole framebuffer
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Render the teapot to the FBO texture
+    renderTeapot();
+
+    // Render back on screen
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+/**
+ * @brief drawBox
+ */
+void drawBox()
+{
+    glBegin(GL_QUADS);
+
+    //////////////////////////////////////////////////////////////// Front Face
+    // Bottom Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+
+    // Bottom Right Of The Texture and Quad`
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+
+    // Top Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+
+    // Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    //////////////////////////////////////////////////////////////// Back Face
+    // Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    // Top Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+
+    // Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+
+    // Bottom Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    //////////////////////////////////////////////////////////////// Top Face
+    // Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+
+    // Bottom Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+
+    // Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+
+    // Top Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    //////////////////////////////////////////////////////////////// Bottom Face
+    // Top Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    // Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+
+    // Bottom Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+
+    // Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    //////////////////////////////////////////////////////////////// Right face
+    // Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+
+    // Top Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+
+    // Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+
+    // Bottom Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    //////////////////////////////////////////////////////////////// Left Face
+    // Bottom Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    // Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+
+    // Top Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+
+    // Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+
+    glEnd();
+}
+
+/**
+ * @brief drawQuad
+ */
+void drawQuad()
+{
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  0.0f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  0.0f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  0.0f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  0.0f);
+    glEnd();
+}
+
+/**
+ * @brief renderTextureToGeometry
+ */
+void renderTextureToGeometry()
+{
+    // Render the teapot to the FBO
+    renderToFBO();
+
+    // Clear color of the FBO
+    glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+
+    // Clear Screen And Depth Buffer on the framebuffer to black
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Enable the 2D texturing
+    glEnable(GL_TEXTURE_2D);
+
+    // Bind our FBO texture
+    glBindTexture(GL_TEXTURE_2D, fboTexId);
+
+    // Set The Current Viewport
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Reset The Modelview Matrix
+    glLoadIdentity();
+
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glRotatef(xRotCube, 1.0f, 0.0f, 0.0f);
+    glRotatef(yRotCube, 0.0f, 1.0f, 0.0f);
+    glRotatef(zRotCube, 0.0f, 0.0f, 1.0f);
+    glColor3f(1, 1, 1);
+
+    // glScalef(0.5, 0.5, 0.5);
+
+    if (renderOnbox) {
+        // Texture the CUBE with the teapot texture
+        drawBox();
+    } else {
+        // Texture a quad
+        drawQuad();
+    }
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // Disable the 2D texturing
+    glDisable(GL_TEXTURE_2D);
+
+    // Swap the buffers
+    glutSwapBuffers();
+}
+
+/**
+ * @brief display
+ */
+void display(void)
+{
+    // Render the texture to the cube.
+    renderTextureToGeometry();
+
+    // renderTeapot();
+
+    // Swap the buffers
+    glutSwapBuffers();
 }
 
 /**
@@ -116,21 +358,14 @@ void keyboard(unsigned char key, int x, int y)
     case 'q':
         exit(0);
         break;
+
+    case ' ':
+        renderOnbox = !(renderOnbox);
+        break;
+
     default:
         break;
     }
-}
-
-/**
- * @brief display
- */
-void display(void)
-{
-    // Render the teapot
-    renderTeapot();
-
-    // Swap the buffers
-    glutSwapBuffers();
 }
 
 /**
@@ -163,6 +398,10 @@ void idle(void)
     xRot += 1.0;
     yRot += 1.0;
     zRot += 1.0;
+
+    xRotCube += 1.0;
+    yRotCube += 1.0;
+    zRotCube += 1.0;
 
     // Redisplay
     glutPostRedisplay();
